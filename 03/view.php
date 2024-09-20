b63354f50f1079160b161ad1f358dc0dc652bd33
kenshu <kenshu.app@gmail.com>
2024-09-20 16:35:50 +0900

    fix:03-1 logicView

---
+++ 03/view.php
@@ -0,0 +1,17 @@
+<?php
+$greeting = 'さようなら';
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1><?= $greeting ?></h1>
+</body>
+
+</html>
