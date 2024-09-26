248c9f5e6b2992cd5b41183173fef3aedbe5c521
kenshu <kenshu.app@gmail.com>
2024-09-26 12:58:54 +0900

    fix:10-1 while

---
+++ 09/calcurator.php
@@ -0,0 +1,31 @@
+<?php
+$num1 = '';
+$num2 = '';
+if (!empty($_POST)) {
+    $num1 = $_POST['num1'];
+    $num2 = $_POST['num2'];
+    $result = $num1 + $num2;
+}
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>計算</title>
+</head>
+
+<body>
+    <h1>計算</h1>
+    <?php if ($_SERVER['REQUEST_METHOD'] === 'POST'): ?>
+        <h2><?=htmlspecialchars($num1, ENT_QUOTES | ENT_HTML5, 'UTF-8')?> + <?=htmlspecialchars($num2, ENT_QUOTES | ENT_HTML5, 'UTF-8')?> = <?=$result?></h2>
+    <?php endif; ?>
+    <form action="" method="post">
+        <input type="text" name="num1" value="<?=htmlspecialchars($num1, ENT_QUOTES | ENT_HTML5, 'UTF-8')?>">+
+        <input type="text" name="num2" value="<?=htmlspecialchars($num2, ENT_QUOTES | ENT_HTML5, 'UTF-8')?>">=
+        <p><input type="submit" value="計算"></p>
+    </form>
+</body>
+
+</html>
